1b50c88a1270fccdd52372c8a1aa608a6510e72f
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2025-01-16 01:47:03 -0500

    WIP

--- scripts/installNodeSubModules.sh
+++ scripts/installNodeSubModules.sh
@@ -1,4 +1,19 @@
-echo START INSTALLING NPM Modules in $PWD/spcoin-lib
-cd ./spcoin-lib
-npm i
-cd ..
+exeIfDirExists() {
+    DIRECTORY=$1
+    COMMAND=$2
+    local CURR_DIR=$PWD
+    echo "CURRENT DIRECTORY : $PWD"
+    if [ -d "$DIRECTORY" ]; then
+        echo INSTALLING in: SUB-DIRECTORY $DIRECTORY =====
+        cd $DIRECTORY
+        echo "EXECUTING: $COMMAND"
+        eval $COMMAND
+    else 
+        echo "***ERROR: SUB-DIRECTORY $DIRECTORY NOT FOUND"
+        echo "***ERROR: COMMAND $COMMAND NOT EXECUTED" 
+    fi
+    cd $CURR_DIR
+}
+
+echo "====================== spcoin-common ================================="
+exeIfDirExists ./spcoin-lib "npm i"
